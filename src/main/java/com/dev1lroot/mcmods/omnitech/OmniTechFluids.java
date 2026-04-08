8d663a1bc9c605c381cc8860f5922e456aa90150
Dev1lroot <sfoserson@gmail.com>
2026-04-09 00:12:45 +0200

    Heat and Cold 2.0

--- src/main/java/com/dev1lroot/mcmods/omnitech/OmniTechFluids.java
+++ src/main/java/com/dev1lroot/mcmods/omnitech/OmniTechFluids.java
@@ -32,11 +32,10 @@ public class OmniTechFluids
     public static final FluidObject MOLTEN_BRASS = new FluidObject("molten_brass",
             FluidType.Properties.create().density(8900).viscosity(3000).temperature(1200));
 
-    public static final FluidObject AIR = new FluidObject("air",
-            FluidType.Properties.create().density(-1000).viscosity(10).temperature(10));
-    public static final FluidObject COMPRESSED_HEATED_AIR = new FluidObject("compressed_heated_air",
-            FluidType.Properties.create().density(10).viscosity(10).temperature(300));
-    public static final FluidObject COMPRESSED_AIR = new FluidObject("compressed_air",
+    public static final FluidObject AIR = new FluidObject("air", gasProps());
+    public static final FluidObject COMPRESSED_HEATED_AIR = new FluidObject("compressed_heated_air", gasProps());
+    public static final FluidObject COMPRESSED_AIR = new FluidObject("compressed_air", gasProps());
+    public static final FluidObject LIQUEFIED_AIR = new FluidObject("liquefied_air",
             FluidType.Properties.create().density(10).viscosity(10).temperature(0));
 
     // Вспомогательный метод для свойств газа
